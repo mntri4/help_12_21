28cd7cca03fbe71c417f103d599e92170ca7001f
mntri4 <31458021+mntri4@users.noreply.github.com>
2019-12-21 10:53:24 -0600

    Add files via upload
    Initial load

---
+++ Tabs.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+class Tabs extends React.Component {
+    static defaultProps = {
+      tabs: [],
+    };
+    state = {
+      currentTabIndex: 0
+    };
+    /* not showing all the methods */
+    renderContent() {
+      const currentTab = this.props.tabs[this.state.currentTabIndex]
+      return (
+        <div className='content'>
+          {currentTab.content}
+        </div>
+      )
+    }
+    /* not showing all the methods */
+  }
+   
+  handleButtonClick(index) {
+    this.setState({ currentTabIndex: index })
+  }
+  renderButtons() {
+    return this.props.tabs.map((tab, index) => (
+      <button key={index} onClick={() => this.handleButtonClick(index)}>
+        {tab.name}
+      </button>
+    ))
+  }
+
+export default Tabs;
